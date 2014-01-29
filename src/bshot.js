c3796101ebee28aff4b045691a6d7e2ebcd36366
Zsámboki Roland <zsamboki.roland@gmail.com>
2014-01-29 22:08:22 +0100

    Removed some comments

--- src/bshot.js
+++ src/bshot.js
@@ -56,33 +56,4 @@
 //@import "model.ns.js"
 //@import "factory.js"
 //@import "generator.js"
-
-/*
-  // Collection method.
-  $.fn.awesome = function() {
-    return this.each(function(i) {
-      // Do something awesome to each selected element.
-      $(this).html('awesome' + i);
-    });
-  };
-
-  // Static method.
-  $.awesome = function(options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.awesome.options, options);
-    // Return something awesome.
-    return 'awesome' + options.punctuation;
-  };
-
-  // Static method default options.
-  $.awesome.options = {
-    punctuation: '.'
-  };
-
-  // Custom selector.
-  $.expr[':'].awesome = function(elem) {
-    // Is this element awesome?
-    return $(elem).text().indexOf('awesome') !== -1;
-  };
-*/
 }(jQuery));
